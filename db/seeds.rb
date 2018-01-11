7d2185b5e56a3c965bf1d0f1f2cefc36a1943818
Jacqueline Lee <anginblu@gmail.com>
2018-01-11 18:18:45 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@ set(:song_name) { "That One with the Guitar" }
 set(:genre_1) { Genre.create(name: genre_1_name) }
 set(:genre_2) { Genre.create(name: genre_2_name) }
 @song = Song.create(name: song_name)
+artist = Artist.create(name: artist_name)
+
+@song.song_genres.create(genre: genre_1)
+@song.artist = artist
+
+@song.save
