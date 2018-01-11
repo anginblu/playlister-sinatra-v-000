b45927a987ebc2b0cbdd4bbbb225c48e0c5bf71f
Jacqueline Lee <anginblu@gmail.com>
2018-01-11 18:19:45 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
-set(:artist_name) { "Person with a Face" }
-set(:genre_1_name) { "Hippity Hop" }
-set(:genre_2_name) { "New Age Garbage" }
-set(:song_name) { "That One with the Guitar" }
-set(:genre_1) { Genre.create(name: genre_1_name) }
-set(:genre_2) { Genre.create(name: genre_2_name) }
+artist_name = "Person with a Face"
+genre_1_name = "Hippity Hop" 
+genre_2_name = "New Age Garbage"
+song_name = "That One with the Guitar"
+genre_1 = Genre.create(name: genre_1_name)
+genre_2 - Genre.create(name: genre_2_name)
 
 @song = Song.create(name: song_name)
 artist = Artist.create(name: artist_name)
