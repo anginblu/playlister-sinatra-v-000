f3a3a90224df940fab98af6942c9c6be2e2c48c6
Jacqueline Lee <anginblu@gmail.com>
2018-01-11 19:55:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -63,8 +63,6 @@ class SongsController < ApplicationController
       @artist = Artist.create(name: params["Artist Name"])
       @song.artist = @artist
       @song.save
-    else
-      @song = Song.find_by(name: params["Artist Name"])
     end
 
     if params["genre_name"].empty?
