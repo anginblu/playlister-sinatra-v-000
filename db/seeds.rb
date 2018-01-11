f717778de5571c468b4a7059a78fa85ec0ab70b0
Jacqueline Lee <anginblu@gmail.com>
2018-01-11 18:17:15 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+let(:artist_name) { "Person with a Face" }
+let(:genre_1_name) { "Hippity Hop" }
+let(:genre_2_name) { "New Age Garbage" }
+let(:song_name) { "That One with the Guitar" }
+let!(:genre_1) { Genre.create(name: genre_1_name) }
+let!(:genre_2) { Genre.create(name: genre_2_name) }
